b33365a345a3ec2debfa80a3b6f287ff4ff5b1dc
Trung Nguyen <trung.nguyen106@hcmut.edu.vn>
2024-01-06 08:36:34 +0700

    Create daily-icafe-setup.sh

---
+++ daily-icafe-setup.sh
@@ -0,0 +1,4 @@
+echo 'https://fastdl.mongodb.org/windows/mongodb-windows-x86_64-6.0.12-signed.msi'
+echo 'https://nodejs.org/download/release/v18.19.0/node-v18.19.0-x64.msi'
+echo 'https://code.visualstudio.com/docs/?dv=win64user'
+echo 'https://github.com/git-for-windows/git/releases/download/v2.43.0.windows.1/Git-2.43.0-64-bit.exe'
